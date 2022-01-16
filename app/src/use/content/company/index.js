ab5ee1eae81fa8fe0cd18a615230c62aa22c654a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-17 00:48:36 +0300

    git init

---
+++ app/src/use/content/company/index.js
@@ -0,0 +1,21 @@
+const timeLine = [
+    {
+        id: 1,
+        date: '1 января',
+        event: 'открытие',
+    },
+    {
+        id: 1,
+        date: '1 января',
+        event: 'открытие',
+    },
+    {
+        id: 1,
+        date: '1 января',
+        event: 'открытие',
+    },
+]
+
+export const useTimeLine = {
+    timeLine
+}
