a154469a42409edf9db56a8c0d910a01b6f916ba
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-18 19:55:09 +0300

    git init

--- app/src/routes/testimonials/index.svelte
+++ app/src/routes/testimonials/index.svelte
@@ -387,7 +387,7 @@
                                 </div>
                                 <div class="mt-6">
                                     <a href="#" class="cursor-not-allowed w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
-                                        Смотреть все
+                                        Оставить отзыв
                                     </a>
                                 </div>
                             </div>
---
+++ app/src/use/content/services/index.js
@@ -0,0 +1,141 @@
+const prices = [
+    {
+        id: 1,
+        name: 'Косметический',
+        price: '2500',
+        description: 'some text',
+        works: [
+            {
+                id: 1,
+                name: 'Демонтаж'
+            },
+        ]
+    },
+    {
+        id: 1,
+        name: 'Стандартный',
+        price: '4500',
+        description: '',
+        works: [
+            {
+                id: 1,
+                name: ''
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Капитальный',
+        price: '8500',
+        description: '',
+        works: [
+            {
+                id: 1,
+                name: ''
+            }
+        ]
+    },
+]
+
+const services = [
+    {
+        id: 1,
+        name: 'Демонтаж',
+        description: ''
+    },
+    {
+        id: 2,
+        name: 'Возведение перегородок',
+        description: ''
+    },
+    {
+        id: 3,
+        name: 'Электромонтажные работы',
+        description: ''
+    },
+    {
+        id: 4,
+        name: 'Заливка стяжки пола',
+        description: ''
+    },
+    {
+        id: 5,
+        name: 'Монтаж инженерной сантехники',
+        description: ''
+    },
+    {
+        id: 6,
+        name: 'Штукатурка стен',
+        description: ''
+    },
+    {
+        id: 7,
+        name: 'Укладка плитки санузлов',
+        description: ''
+    },
+    {
+        id: 8,
+        name: 'Монтаж конструкций ГКЛ',
+        description: ''
+    },
+    {
+        id: 9,
+        name: 'Заливка наливных полов',
+        description: ''
+    },
+    {
+        id: 10,
+        name: 'Укладка плитки пола',
+        description: ''
+    },
+    {
+        id: 11,
+        name: 'Шпатлевка стен и потолков',
+        description: ''
+    },
+    {
+        id: 12,
+        name: 'Покраска потолков',
+        description: ''
+    },
+    {
+        id: 13,
+        name: 'Укладка напольного покрытия',
+        description: ''
+    },
+    {
+        id: 14,
+        name: 'Поклейка обоев',
+        description: ''
+    },
+    {
+        id: 15,
+        name: 'Установка дверей',
+        description: ''
+    },
+    {
+        id: 16,
+        name: 'Установка оконных блоков',
+        description: ''
+    },
+    {
+        id: 17,
+        name: 'Монтаж плинтусов и багета',
+        description: ''
+    },
+    {
+        id: 18,
+        name: 'Монтаж бытовой сантехники',
+        description: ''
+    },
+    {
+        id: 19,
+        name: 'Монтаж натяжных потолков',
+        description: ''
+    },
+]
+
+export const useServices = {
+    price,
+    services
+}
