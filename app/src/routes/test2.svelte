30de92bd5dde98c6c7eb0d27fe3f12d23d7c5b73
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-24 16:51:27 +0300

    git init

--- app/src/routes/test2.svelte
+++ app/src/routes/test2.svelte
@@ -162,3 +162,102 @@
         </div>
     </div>
 </section>
+
+
+
+<div
+        class="relative p-6 mx-0 mt-4 mb-0 leading-4 text-left text-black rounded"
+        style="box-shadow: var(--page-block-shadow);"
+>
+    <div class="text-left">
+        <div class="text-black">
+            <div class="mb-6 text-center">
+                <div class="text-xl leading-6">Впервые ВКонтакте?</div>
+                <div class="mt-px text-xs">Моментальная регистрация</div>
+                <div class="hidden mt-2 text-sm"></div>
+            </div>
+
+            <form class="p-0 m-0">
+                <div class="">
+                    <input
+                            type="text"
+                            id="join_phone"
+                            class="px-3 pt-1 pb-2 m-0 w-full h-8 font-sans text-sm rounded-sm shadow-none appearance-none cursor-text box-border"
+                            name="phone"
+                            value=""
+                            placeholder="Ваш телефон"
+                            style="outline: 0px;"
+                    />
+                </div>
+
+                <div class="mt-1"></div>
+
+                <button
+                        class="inline-block py-0 px-4 mx-0 mt-2 mb-0 w-full text-sm text-center align-middle rounded border-0 appearance-none cursor-pointer select-none box-border hover:no-underline focus:no-underline"
+                        type="button"
+                        style="background-position: 0px center;"
+                >
+          <span class="flex justify-center items-center h-8 text-white">
+            <span class="mb-px"> Создать аккаунт </span>
+          </span>
+                </button>
+            </form>
+
+            <div
+                    class="block overflow-hidden h-12 opacity-100 box-border"
+                    style="opacity: 1; height: 55px; overflow: hidden; display: block;"
+            >
+                <div
+                        class="block absolute w-4 h-4 leading-9 text-center rounded-sm cursor-pointer box-border"
+                        data-use-continue-as="true"
+                        data-width="264"
+                        data-max-rows="1"
+                        data-size="medium"
+                        data-button-type="continue_with"
+                        data-placeholder="Войти через Facebook"
+                        login_text=""
+                        fb-xfbml-state="rendered"
+                        fb-iframe-plugin-query="app_id=128749580520227&amp;button_type=continue_with&amp;container_width=270&amp;locale=ru_RU&amp;login_text=&amp;max_rows=1&amp;sdk=joey&amp;size=medium&amp;use_continue_as=true&amp;width=264"
+                        style='font-size: 0px; content: ""; top: 9px;'
+                >
+          <span
+                  style="vertical-align: bottom; width: 0px; height: 0px; font-size: 0px;"
+                  class="hidden relative w-0 h-0 text-justify align-bottom"
+          ><iframe
+                  name="f3bef690522592c"
+                  width="264px"
+                  height="1000px"
+                  data-testid="fb:login_button Facebook Social Plugin"
+                  title="fb:login_button Facebook Social Plugin"
+                  frameborder="0"
+                  allowtransparency="true"
+                  allowfullscreen="true"
+                  scrolling="no"
+                  allow="encrypted-media"
+                  src="https://www.facebook.com/v2.7/plugins/login_button.php?app_id=128749580520227&amp;button_type=continue_with&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df246f5b8981970c%26domain%3Dvk.com%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fvk.com%252Ff2cf92dd14c563c%26relation%3Dparent.parent&amp;container_width=270&amp;locale=ru_RU&amp;login_text=&amp;max_rows=1&amp;sdk=joey&amp;size=medium&amp;use_continue_as=true&amp;width=264"
+                  class="absolute visible w-0 h-0"
+                  style="border: none; visibility: visible; width: 0px; height: 0px; font-size: 0px;"
+          ></iframe
+          ></span>
+                </div>
+            </div>
+
+            <div class="mt-4 text-center">
+                Нажимая «Создать аккаунт», вы принимаете
+                <a
+                        href="https://id.vk.com/terms"
+                        target="_blank"
+                        class="no-underline cursor-pointer hover:underline"
+                >пользовательское соглашение</a
+                >
+                и
+                <a
+                        href="https://id.vk.com/privacy"
+                        target="_blank"
+                        class="no-underline cursor-pointer hover:underline"
+                >политику конфиденциальности</a
+                >.
+            </div>
+        </div>
+    </div>
+</div>
