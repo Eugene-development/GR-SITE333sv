05c1e2732236665a4ae45d615dbc6b9ece70d665
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-24 22:14:55 +0300

    git init

--- app/src/lib/header/Header.svelte
+++ app/src/lib/header/Header.svelte
@@ -3,6 +3,7 @@
     import {useHeader} from "../../use/content/header";
     // import { useTest } from "../../hooks";
     import {useVisible} from "../../use/visible";
+    import MobileMenu from "./mobile/index.svelte"
 
     const {visible} = useVisible;
     const changeVisibleFormMeasurement = () => bool.update(visible)
@@ -77,61 +78,7 @@
             </nav>
         </div>
 
-        <!--
-          Mobile menu, show/hide based on menu open state.
-
-          Entering: "duration-150 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-          Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-        {#if false}
-            <div class="absolute z-20 top-0 inset-x-0 p-2 transition transform origin-top lg:hidden bg-gray-50">
-                  <div class="rounded-lg shadow-md  ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div class="px-5 pt-4 flex items-center justify-between">
-                      <div>
-                        <img class="h-8 w-auto" src="/Logo/logo1.svg"
-                             alt="">
-                      </div>
-                      <div class="-mr-2">
-                        <button type="button"
-                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
-                          <span class="sr-only">Close menu</span>
-                          <!-- Heroicon name: outline/x -->
-                          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                               stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
-                    <div class="pt-5 pb-6">
-                      <div class="px-2 space-y-1">
-                        <a href="services" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Услуги</a>
-
-                        <a href="company" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Компания</a>
-
-                        <a href="gallery" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Галерея</a>
-
-                        <a href="testimonials" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Отзывы</a>
-
-                        <a href="specialists" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Специалисты</a>
-
-                        <a href="contacts" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Контакты</a>
-                      </div>
-                      <div class="mt-6 px-5">
-                        <button on:click={changeVisibleFormMeasurement} type="button"
-                                class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-green-500 to-cyan-600 text-white font-medium hover:from-green-600 hover:to-cyan-700">Получить консультацию</button>
-                      </div>
-                      <div class="mt-6 px-5">
-                        <p class="text-center text-base font-medium text-gray-500">Услуга бесплатная</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-        {/if}
+        <MobileMenu/>
     </div>
 
 
