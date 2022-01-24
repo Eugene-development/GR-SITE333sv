0f1597bcd1338b22c6cae56cd7ce0fe4ae1b91a3
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-24 19:32:01 +0300

    git init

--- app/src/lib/header/Header.svelte
+++ app/src/lib/header/Header.svelte
@@ -30,23 +30,6 @@
 
 </script>
 
-<!--<button on:click={increment}>-->
-<!--    +-->
-<!--</button>-->
-<!--<br>-->
-
-
-<!--<h1>Счётчик равен {bool_value}</h1>-->
-
-
-
-
-<!--<button on:click={consoleTest}>-->
-<!--    CONSOLE-->
-<!--</button>-->
-<!--{result}-{result2}-->
-
-<!--<pre> {ggg}</pre>-->
 <header>
 
     <div class="relative">
@@ -104,47 +87,49 @@
             From: "opacity-100 scale-100"
             To: "opacity-0 scale-95"
         -->
-        <!--        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">-->
-        <!--          <div class="rounded-lg shadow-md  ring-1 ring-black ring-opacity-5 overflow-hidden">-->
-        <!--            <div class="px-5 pt-4 flex items-center justify-between">-->
-        <!--              <div>-->
-        <!--                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-green-500-cyan-600.svg"-->
-        <!--                     alt="">-->
-        <!--              </div>-->
-        <!--              <div class="-mr-2">-->
-        <!--                <button type="button"-->
-        <!--                        class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">-->
-        <!--                  <span class="sr-only">Close menu</span>-->
-        <!--                  &lt;!&ndash; Heroicon name: outline/x &ndash;&gt;-->
-        <!--                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"-->
-        <!--                       stroke="currentColor" aria-hidden="true">-->
-        <!--                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>-->
-        <!--                  </svg>-->
-        <!--                </button>-->
-        <!--              </div>-->
-        <!--            </div>-->
-        <!--            <div class="pt-5 pb-6">-->
-        <!--              <div class="px-2 space-y-1">-->
-        <!--                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Product</a>-->
+                <div class="absolute z-20 top-0 inset-x-0 p-2 transition transform origin-top md:hidden bg-gray-50">
+                  <div class="rounded-lg shadow-md  ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div class="px-5 pt-4 flex items-center justify-between">
+                      <div>
+                        <img class="h-8 w-auto" src="./static/Logo/logo1.svg"
+                             alt="">
+                      </div>
+                      <div class="-mr-2">
+                        <button type="button"
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                          <span class="sr-only">Close menu</span>
+                          <!-- Heroicon name: outline/x -->
+                          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                               stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="pt-5 pb-6">
+                      <div class="px-2 space-y-1">
+                        <a href="services" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Услуги</a>
 
-        <!--                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Features</a>-->
+                        <a href="company" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Компания</a>
 
-        <!--                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Marketplace</a>-->
+                        <a href="gallery" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Галерея</a>
 
-        <!--                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Company</a>-->
-        <!--              </div>-->
-        <!--              <div class="mt-6 px-5">-->
-        <!--                <a href="#"-->
-        <!--                   class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-green-500 to-cyan-600 text-white font-medium hover:from-green-600 hover:to-cyan-700">Start-->
-        <!--                  free trial</a>-->
-        <!--              </div>-->
-        <!--              <div class="mt-6 px-5">-->
-        <!--                <p class="text-center text-base font-medium text-gray-500">Existing customer? <a href="#" class="text-gray-900 hover:underline">Login</a>-->
-        <!--                </p>-->
-        <!--              </div>-->
-        <!--            </div>-->
-        <!--          </div>-->
-        <!--        </div>-->
+                        <a href="testimonials" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Отзывы</a>
+
+                        <a href="specialists" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Специалисты</a>
+
+                        <a href="contacts" class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Контакты</a>
+                      </div>
+                      <div class="mt-6 px-5">
+                        <a href="#"
+                           class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-green-500 to-cyan-600 text-white font-medium hover:from-green-600 hover:to-cyan-700">Получить консультацию</a>
+                      </div>
+                      <div class="mt-6 px-5">
+                        <p class="text-center text-base font-medium text-gray-500">Услуга бесплатная</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
     </div>
 
 
