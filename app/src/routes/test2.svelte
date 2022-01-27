035c3c294aae05634bb8f5f1d410b545a0d615b2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-27 19:39:22 +0300

    git init

---
+++ app/src/routes/test2.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    export async function load({fetch}) {
+        const id = 1;
+        // const id = page.params.id;
+        const url = `https://jsonplaceholder.typicode.com/users`;
+        const res = await fetch(url);
+        const post = await res.json();
+        console.log(post)
+        return {props: {post}}
+    }
+</script>
+
+<script>
+    export let post;
+</script>
+
+{#each post as {name}, i }
+    <h2 >{name}</h2>
+{/each}
---
+++ app/src/routes/test3.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    export async function load({params, fetch, session, stuff}) {
+
+        const url = `http://localhost:7751/get-all-category/`;
+        const headers = {
+            Authorization: `Bearer 2`
+        }
+
+        const res = await fetch(url, {
+            headers,
+        });
+        const posts = await res.json();
+        const category = posts.data
+        console.log(category)
+        return {props: {category}}
+    }
+</script>
+
+<script>
+    export let category;
+</script>
+
+{#each category as {name}, i }
+<h2 >{name}</h2>
+    {/each}
