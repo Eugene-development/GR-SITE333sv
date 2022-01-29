74c3a0941e59660ec7fddd11209cf73f4cddeb69
indexpro24@gmail.com <indexpro24@gmail.com>
2022-01-29 17:45:16 +0300

    git init

---
+++ app/src/routes/test4/index.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    export const load = async ({fetch}) => {
+        const res = await fetch("/api/users")
+        const data = await res.json()
+        console.log(data)
+        return {
+            props: {
+                data
+            }
+        }
+    }
+</script>
+
+<script>
+    export let data
+</script>
+
+
+<pre>{JSON.stringify(data)}</pre>
