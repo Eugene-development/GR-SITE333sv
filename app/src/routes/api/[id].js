ea276e585bb144ac6b10dfe405826fd05a066dc7
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-04 19:40:05 +0300

    git init

--- app/src/routes/api/[id].js
+++ app/src/routes/api/[id].js
@@ -1,19 +1,11 @@
 export const get = async ({ params }) => {
-
-    const {id} = params;
-
-    const url = `https://adminexpo.com:7751/get-category/`;
+    const { id } = params;
+    const domain = import.meta.env.VITE_API_CRUD;
+    const url = `${ domain }/get-category/${ id }`;
     const headers = {
-        Authorization: `Bearer 2`
+        Authorization: `Bearer ${ import.meta.env.VITE_TOKEN }`
     }
-    const res = await fetch(url + id, {
-        headers,
-    });
+    const res = await fetch(url, { headers });
     const post = await res.json();
-
-    return {
-        body: {
-            post
-        }
-    }
+    return { body: { post } }
 }
