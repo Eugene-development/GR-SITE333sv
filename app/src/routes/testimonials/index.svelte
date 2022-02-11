0afede0d99d89a3d044502148afba2f397aa42f5
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-11 16:20:31 +0300

    git init

--- app/src/routes/testimonials/index.svelte
+++ app/src/routes/testimonials/index.svelte
@@ -271,32 +271,53 @@
                                     </select>
                                     год:</h2>
                                 <div class="flow-root mt-6">
-                                    <ul role="list" class="-my-5 divide-y divide-gray-200">
+                                    <div role="list" class="-my-5 divide-y divide-gray-200">
                                         {#each testimonials_2022 as {name, stars, text}}
-                                        <li class="py-5">
+
+                                            <div class="py-5">
                                             <div class="relative focus-within:ring-2 focus-within:ring-cyan-500">
-                                                <h3 class="text-sm font-semibold text-gray-800">
-                                                    <p class="">
+                                                <div class="flex text-sm font-semibold text-gray-800">
+                                                    <div>
+                                                        <h3 class="sr-only">Reviews</h3>
+                                                        <div class="flex items-center">
+                                                            {#each Array(stars) as _, row}
+                                                            <svg class="text-yellow-500 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                                            </svg>
+                                                            {/each}
+
+                                                            {#each Array(5 - stars) as _, row}
+                                                            <svg class="text-gray-300 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                                            </svg>
+                                                            {/each}
+
+                                                        </div>
+                                                        <p class="sr-only">{5-stars} out of 5 stars</p>
+                                                    </div>
+
+                                                    <p class="ml-2">
                                                         <!-- Extend touch target to entire panel -->
-                                                        <span class="absolute inset-0" aria-hidden="true"></span>
+                                                        <span class=" absolute inset-0" aria-hidden="true"></span>
                                                         {name}
                                                     </p>
-                                                </h3>
+
+                                                </div>
                                                 <p class="mt-1 text-sm text-gray-600 line-clamp-2">
                                                     {text}
                                                 </p>
                                             </div>
-                                        </li>
+                                        </div>
                                         {:else}
                                             <p>Нет данных!</p>
                                         {/each}
 
-                                    </ul>
+                                    </div>
                                 </div>
                                 <div class="mt-6">
-                                    <a href="#" class="cursor-not-allowed w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+                                    <button class="cursor-not-allowed w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
                                         Просмотреть все
-                                    </a>
+                                    </button>
                                 </div>
 <!--                                <div class="mt-6">-->
 <!--                                    <a href="#" class="cursor-not-allowed w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">-->
