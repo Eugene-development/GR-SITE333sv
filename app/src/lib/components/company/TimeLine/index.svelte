1e6996f5d4ed8e52acea7d9b3feb94d047422817
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-09 20:38:17 +0300

    git init

--- app/src/lib/components/company/TimeLine/index.svelte
+++ app/src/lib/components/company/TimeLine/index.svelte
@@ -15,18 +15,18 @@
                 {#each timeLine as {id, date, event}, i}
                     <div class="relative pb-8">
                         {#if timeLine.length - 1 !== i}
-                        <span class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+                        <span class="absolute top-5 left-3 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
                         {/if}
                         <div class="relative flex space-x-3">
                             <div>
-                              <span class="h-8 w-8 rounded-full bg-green-600 flex items-center justify-center ring-8 ring-white">
+                              <span class="h-6 w-6 rounded-full bg-cyan-600 flex items-center justify-center ring-8 ring-white">
                                 <!-- Heroicon name: solid/check -->
-                                <svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                   <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
                                 </svg>
                               </span>
                             </div>
-                            <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                            <div class="min-w-0 flex-1 flex justify-between space-x-4">
                                 <div>
                                     <p class="text-sm text-gray-500">{event}</p>
                                 </div>
